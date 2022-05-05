eefa6f361ae23ae371b8f5c714c21135c0f6e5d8
LeeJunho <104730420+CodeAlphas@users.noreply.github.com>
2022-05-05 20:14:31 +0900

    Add files via upload

---
+++ BJ_2438.py
@@ -0,0 +1,12 @@
+"""
+ 2438번: 별 찍기 - 1
+ Problem Source: https://www.acmicpc.net/problem/2438
+ Version: Python 3.10.4
+
+ Created by CodeAlphas on 2022/05/05.
+"""
+
+n = int(input())
+
+for i in range(1, n+1):
+    print("*" * i)
