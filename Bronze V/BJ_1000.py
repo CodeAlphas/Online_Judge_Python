380b2fdbddb824932990260917a875a1be33bf38
LeeJunho <104730420+CodeAlphas@users.noreply.github.com>
2022-05-08 10:55:53 +0900

    [ADD] 풀이 추가

---
+++ Bronze V/BJ_1000.py
@@ -0,0 +1,11 @@
+"""
+ 1000번: A+B
+ Problem Source: https://www.acmicpc.net/problem/1000
+ Version: Python 3.10.4
+
+ Created by CodeAlphas on 2022/04/15.
+"""
+
+a, b = map(int, input().split())
+
+print(a+b)
