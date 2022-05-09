b74ea7c5618785a8a9273f9bbb601ff565219cfa
LeeJunho <104730420+CodeAlphas@users.noreply.github.com>
2022-05-10 08:38:00 +0900

    [ADD] 레포지토리 정리

--- 백준/Bronze V/BJ_1000.py
+++ 백준/Bronze V/BJ_1000.py
@@ -1,11 +1,10 @@
 """
- 1000번: A+B
+ 백준 1000번: A+B 풀이
+ Author: CodeAlphas
+ Date: 2022/04/15
  Problem Source: https://www.acmicpc.net/problem/1000
  Version: Python 3.10.4
-
- Created by CodeAlphas on 2022/04/15.
 """
 
 a, b = map(int, input().split())
-
 print(a+b)
